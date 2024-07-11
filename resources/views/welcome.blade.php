b05dccca5ebaa1594ff249f429498ee2cc5e8666
Narek <narsahakyan.work@gmail.com>
2024-07-11 09:59:06 +0400

    changes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -28,12 +28,6 @@
                     @auth
                         <a href="{{ url('/home') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Home</a>
                     @else
-                        <a
-                            href="{{ route('auth.google.redirect') }}"
-                            class="btn btn-primary btn-sm"
-                        >
-                            Login with Google
-                        </a>
                         <a href="{{ route('login') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Log in</a>
 
                         @if (Route::has('register'))
