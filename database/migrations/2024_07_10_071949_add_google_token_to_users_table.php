c0a264dfe2941c94fd0e84262f2283f05996bdf4
Narek <narsahakyan.work@gmail.com>
2024-07-12 12:26:44 +0400

    remove unused fields

--- database/migrations/2024_07_10_071949_add_google_token_to_users_table.php
+++ database/migrations/2024_07_10_071949_add_google_token_to_users_table.php
@@ -13,7 +13,6 @@ return new class extends Migration
     {
         Schema::table('users', function (Blueprint $table) {
             $table->text('google_token')->nullable();
-            $table->text('google_refresh_token')->nullable();
         });
     }
 
@@ -23,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->dropColumn(['google_refresh_token', 'google_token']);
+            $table->dropColumn(['google_token']);
         });
     }
 };
